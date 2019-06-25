bb5a8e5c9fcb089e4e3dc37cda6421c131738f7b
Tobias Famos <tfamos@gmx.ch>
2019-06-25 11:44:03 +0200

    Added two unit Tests to check for the Components undone and done. Both fail

--- test/unit/test/TodoList.test.js
+++ test/unit/test/TodoList.test.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
-import { shallowMount } from '@vue/test-utils'
+import { mount, shallowMount } from '@vue/test-utils'
 import TodoApp from "../../../src/components/TodoApp";
+import Undone from "../../../src/components/Undone";
+import Done from "../../../src/components/Done";
 
 
 describe('TodoApp.vue', () => {
@@ -11,7 +13,17 @@ describe('TodoApp.vue', () => {
       }
     }});
     expect(wrapper.find('h1').text()).toEqual("Todo App")
-  })
-})
+  });
+
+  it('should have a component undone', () => {
+    const wrapper = mount(TodoApp,);
+    expect(wrapper.find(Undone)).toHaveLength(1);
+  });
+
+  it('should have a component done',() => {
+    const wrapper = mount(TodoApp);
+    expect(wrapper.find(Done)).toHaveLength(1);
+  });
+});
 
 
